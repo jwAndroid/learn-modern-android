bef915e045558b1d864b7a9c2e4e44b451e35c7f
jwAndroid <cjd9408er@naver.com>
2022-01-28 15:58:10 +0900

    First Learn Android

---
+++ app/src/main/java/com/jw_android/learn_modern_android/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.jw_android.learn_modern_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jw_android.learn_modern_android.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
