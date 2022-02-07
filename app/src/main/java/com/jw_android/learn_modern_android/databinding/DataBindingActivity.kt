4d53868ed55cf6650e321944dfcbc541939d2537
jwAndroid <cjd9408er@naver.com>
2022-02-07 22:21:08 +0900

    Update note

--- app/src/main/java/com/jw_android/learn_modern_android/databinding/DataBindingActivity.kt
+++ app/src/main/java/com/jw_android/learn_modern_android/databinding/DataBindingActivity.kt
@@ -8,5 +8,10 @@ class DataBindingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityDataBindingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // 이방법보다는 원래작성하던 바인딩으로 진행하는게 좋은듯
+        // xml 에 접근할떄 전에 쓰던방식이 편함
+        // 그리고 전에쓰던방식이랑 이방법이랑 어떤차이가 있는지도 알아놓는게 좋을듯함
+
     }
 }
