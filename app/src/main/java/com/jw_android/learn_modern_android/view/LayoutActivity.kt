1f4793f252564891d3c59bc1a5d12cce1e1d510d
jwandroid <cjd9408er@naver.com>
2022-02-19 02:04:06 +0900

    update BaseActivity.kt

---
+++ app/src/main/java/com/jw_android/learn_modern_android/view/LayoutActivity.kt
@@ -0,0 +1,26 @@
+package com.jw_android.learn_modern_android.view
+
+import androidx.databinding.DataBindingUtil
+import com.jw_android.learn_modern_android.R
+import com.jw_android.learn_modern_android.databinding.ActivityLayoutBinding
+import com.jw_android.learn_modern_android.view.base.BaseActivity
+
+class LayoutActivity : BaseActivity<ActivityLayoutBinding>(R.layout.activity_layout) {
+
+
+    override fun initView() {
+
+        binding.apply {
+            button.setOnClickListener {
+                button.text = "modern android"
+            }
+        }
+
+    }
+
+    override fun initData() {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/jw_android/learn_modern_android/view/base/BaseActivity.kt
@@ -0,0 +1,27 @@
+package com.jw_android.learn_modern_android.view.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import com.jw_android.learn_modern_android.R
+import com.jw_android.learn_modern_android.databinding.ActivityLayoutBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(@LayoutRes private val layoutResId: Int) : AppCompatActivity() {
+
+    protected abstract fun initView()
+    protected abstract fun initData()
+
+    protected val binding : T by lazy {
+        DataBindingUtil.setContentView(this , R.layout.activity_layout)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initView()
+        initData()
+    }
+
+
+}
