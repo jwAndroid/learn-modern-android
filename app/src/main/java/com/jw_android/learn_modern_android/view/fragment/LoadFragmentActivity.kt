d004ec53572c1af0b07099349af7d9dc9ad563c3
jwandroid <cjd9408er@naver.com>
2022-04-21 05:32:39 +0900

    fragment setup

---
+++ app/src/main/java/com/jw_android/learn_modern_android/view/fragment/LoadFragmentActivity.kt
@@ -0,0 +1,12 @@
+package com.jw_android.learn_modern_android.view.fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jw_android.learn_modern_android.R
+
+class LoadFragmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_load_fragment)
+    }
+}
