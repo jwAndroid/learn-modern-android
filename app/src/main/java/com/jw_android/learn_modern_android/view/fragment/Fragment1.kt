a5565904fdca2c8a03a7fdc03d261652e397771e
jwAndroid <cjd9408er@naver.com>
2022-04-26 14:49:53 +0900

    learn fragment

---
+++ app/src/main/java/com/jw_android/learn_modern_android/view/fragment/Fragment1.kt
@@ -0,0 +1,19 @@
+package com.jw_android.learn_modern_android.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jw_android.learn_modern_android.R
+
+class Fragment1 : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_1, container, false)
+    }
+
+}
--- app/src/main/java/com/jw_android/learn_modern_android/view/fragment/LoadFragmentActivity.kt
+++ app/src/main/java/com/jw_android/learn_modern_android/view/fragment/LoadFragmentActivity.kt
@@ -1,12 +1,78 @@
 package com.jw_android.learn_modern_android.view.fragment
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.jw_android.learn_modern_android.R
+import com.jw_android.learn_modern_android.databinding.ActivityLoadFragmentBinding
+import com.jw_android.learn_modern_android.view.base.BaseActivity
 
-class LoadFragmentActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_load_fragment)
+private const val TAG_FRAGMENT1 = "fragment1"
+private const val TAG_FRAGMENT2 = "fragment2"
+private const val TAG_FRAGMENT3 = "fragment3"
+
+class LoadFragmentActivity : BaseActivity<ActivityLoadFragmentBinding>(R.layout.activity_load_fragment) {
+    override fun initView() {
+        binding.apply {
+            btnFragment1.setOnClickListener {
+                setFragment(TAG_FRAGMENT1, Fragment1())
+            }
+
+            btnFragment1.setOnClickListener {
+                setFragment(TAG_FRAGMENT2, Fragment2())
+            }
+
+            btnFragment3.setOnClickListener {
+                setFragment(TAG_FRAGMENT3, Fragment3())
+            }
+        }
+
+        setFragment(TAG_FRAGMENT1, Fragment1())
+
     }
+
+    override fun initData() {}
+
+    private fun setFragment(tag: String, fragment: Fragment) {
+        val manager: FragmentManager = supportFragmentManager
+        val ft: FragmentTransaction = manager.beginTransaction()
+
+        if (manager.findFragmentByTag(tag) == null) {
+            ft.add(R.id.frame_container, fragment, tag)
+        }
+
+        val fragment1 = manager.findFragmentByTag(TAG_FRAGMENT1)
+        val fragment2 = manager.findFragmentByTag(TAG_FRAGMENT2)
+        val fragment3 = manager.findFragmentByTag(TAG_FRAGMENT3)
+
+        if (fragment1 != null) {
+            ft.hide(fragment1)
+        }
+        if (fragment2 != null) {
+            ft.hide(fragment2)
+        }
+        if (fragment3 != null) {
+            ft.hide(fragment3)
+        }
+
+        if (tag == TAG_FRAGMENT1) {
+            if (fragment1 != null) {
+                ft.show(fragment1)
+            }
+        }
+        if (tag == TAG_FRAGMENT2) {
+
+            if (fragment2 != null) {
+                ft.show(fragment2)
+            }
+        }
+        if (tag == TAG_FRAGMENT3) {
+            if (fragment3 != null) {
+                ft.show(fragment3)
+            }
+        }
+
+        ft.commitAllowingStateLoss()
+    }
+
 }
